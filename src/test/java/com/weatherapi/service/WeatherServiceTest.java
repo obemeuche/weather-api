bef0a332bee2de99ccfaecd3c0a1157fec2641e5
obemeuche <obemeuchechi@gmail.com>
2026-03-29 23:47:36 +0100

    add import

--- src/test/java/com/weatherapi/service/WeatherServiceTest.java
+++ src/test/java/com/weatherapi/service/WeatherServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
